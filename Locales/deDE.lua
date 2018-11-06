a9d2233fb01676f18c808a07c9d5ff7c1f4b1048
funkydude <funkydude@users.noreply.github.com>
2018-11-06 20:30:14 +0000

    Update deDE, closes #17

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -13,18 +13,18 @@ L.hasTakenTheTrigger = "eingenommen!"
 L.droppedTrigger = "fallen lassen!"
 L.capturedTheTrigger = "errungen!"
 
---L.hordeGate = "Horde Gate"
---L.allianceGate = "Alliance Gate"
+L.hordeGate = "Hordentor"
+L.allianceGate = "Allianztor"
 --L.hordeBoss = "Horde Boss"
 --L.allianceBoss = "Alliance Boss"
 --L.galvangar = "Galvangar"
---L.balinda = "Balinda Stonehearth"
+L.balinda = "Balinda Steinbruch" -- Hauptmann Balinda Steinbruch
 
 --- Alliance IoC Workshop yells:
--- Gnomish Mechanic yells: I'm halfway there! Keep the Horde away from here.  They don't teach fighting in engineering school!
--- Gnomish Mechanic yells: It's broken already?! No worries. It's nothing I can't fix.
+-- Gnomenmechaniker: Ich hab's gleich! Haltet die Horde von hier fern. Kämpfen stand in der Ingenieursschule nicht auf dem
+-- Gnomenmechaniker: Es ist schon kaputt?! Ach, keine Sorge, nichts, was ich nicht reparieren kann.
 --- Horde IoC Workshop yells:
--- Goblin Mechanic yells: I'm about halfway done! Keep the Alliance away - fighting's not in my contract!
--- Goblin Mechanic yells: It's broken again?! I'll fix it... just don't expect the warranty to cover this.
---L.halfway = "halfway"
---L.broken = "broken"
+-- Goblinmechaniker: Ich hab's gleich! Haltet mir die Allianz vom Leib. Kämpfen steht nicht in meinem Vertrag!
+-- Goblinmechaniker: Schon wieder kaputt?! Ich werde es richten... Ihr solltet allerdings nicht davon ausgehen, dass das noch unter die Garantie
+L.halfway = "gleich!"
+L.broken = "kaputt?"
