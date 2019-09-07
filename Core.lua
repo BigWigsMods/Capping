a9744f223130e622c3afc37d96ffa2ae75ce9a75
funkydude <funkydude@users.noreply.github.com>
2019-09-07 21:40:37 +0100

    Modules/IsleOfConquest: Move to its own module

--- Core.lua
+++ Core.lua
@@ -217,6 +217,148 @@ do
 	end
 
 	do
+		--local unitTable1 = {
+		--	"target", "targettarget",
+		--	"mouseover", "mouseovertarget",
+		--	"focus", "focustarget",
+		--	"nameplate1", "nameplate2", "nameplate3", "nameplate4", "nameplate5", "nameplate6", "nameplate7", "nameplate8", "nameplate9", "nameplate10",
+		--	"nameplate11", "nameplate12", "nameplate13", "nameplate14", "nameplate15", "nameplate16", "nameplate17", "nameplate18", "nameplate19", "nameplate20",
+		--	"nameplate21", "nameplate22", "nameplate23", "nameplate24", "nameplate25", "nameplate26", "nameplate27", "nameplate28", "nameplate29", "nameplate30",
+		--	"nameplate31", "nameplate32", "nameplate33", "nameplate34", "nameplate35", "nameplate36", "nameplate37", "nameplate38", "nameplate39", "nameplate40",
+		--}
+		--local unitTable2 = {
+		--	"nameplate1target", "nameplate2target", "nameplate3target", "nameplate4target", "nameplate5target",
+		--	"nameplate6target", "nameplate7target", "nameplate8target", "nameplate9target", "nameplate10target",
+		--	"nameplate11target", "nameplate12target", "nameplate13target", "nameplate14target", "nameplate15target",
+		--	"nameplate16target", "nameplate17target", "nameplate18target", "nameplate19target", "nameplate20target",
+		--	"nameplate21target", "nameplate22target", "nameplate23target", "nameplate24target", "nameplate25target",
+		--	"nameplate26target", "nameplate27target", "nameplate28target", "nameplate29target", "nameplate30target",
+		--	"nameplate31target", "nameplate32target", "nameplate33target", "nameplate34target", "nameplate35target",
+		--	"nameplate36target", "nameplate37target", "nameplate38target", "nameplate39target", "nameplate40target",
+		--}
+		--local unitTable3 = {
+		--	"raid1target", "raid2target", "raid3target", "raid4target", "raid5target",
+		--	"raid6target", "raid7target", "raid8target", "raid9target", "raid10target",
+		--	"raid11target", "raid12target", "raid13target", "raid14target", "raid15target",
+		--	"raid16target", "raid17target", "raid18target", "raid19target", "raid20target",
+		--	"raid21target", "raid22target", "raid23target", "raid24target", "raid25target",
+		--	"raid26target", "raid27target", "raid28target", "raid29target", "raid30target",
+		--	"raid31target", "raid32target", "raid33target", "raid34target", "raid35target",
+		--	"raid36target", "raid37target", "raid38target", "raid39target", "raid40target"
+		--}
+		--local collection, reset, blocked, started = {}, {}, {}, false
+		--local count1, count2, count3 = #unitTable1, #unitTable2, #unitTable3
+		--local UnitGUID, strsplit = UnitGUID, strsplit
+		--
+		--local function parse2()
+		--	for i = 1, count2 do
+		--		local unit = unitTable2[i]
+		--		local guid = UnitGUID(unit)
+		--		if guid then
+		--			local _, _, _, _, _, strid = strsplit("-", guid)
+		--			if strid and collection[strid] and not blocked[strid] then
+		--				blocked[strid] = true
+		--				local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
+		--				SendAddonMessage("Capping", format("%s:%.1f", strid, hp), "INSTANCE_CHAT")
+		--			end
+		--		end
+		--	end
+		--end
+		--local function parse3()
+		--	for i = 1, count3 do
+		--		local unit = unitTable3[i]
+		--		local guid = UnitGUID(unit)
+		--		if guid then
+		--			local _, _, _, _, _, strid = strsplit("-", guid)
+		--			if strid and collection[strid] and not blocked[strid] then
+		--				blocked[strid] = true
+		--				local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
+		--				SendAddonMessage("Capping", format("%s:%.1f", strid, hp), "INSTANCE_CHAT")
+		--			end
+		--		end
+		--	end
+		--end
+		--local function HealthScan()
+		--	local _, _, _, id = UnitPosition("player")
+		--	if id == 30 or id == 628 then -- Alterac Valley, IoC
+		--		Timer(1, HealthScan)
+		--		Timer(0.1, parse2) -- Break up parsing
+		--		Timer(0.2, parse3)
+		--	else
+		--		started = false
+		--		collection, reset = {}, {}
+		--		return
+		--	end
+		--
+		--	for id, counter in next, reset do
+		--		reset[id] = counter + 1
+		--		if counter > 20 then
+		--			local tbl = collection[id]:Get("capping:hpdata")
+		--			collection[id]:Stop()
+		--			reset[id] = nil
+		--			collection[id] = tbl
+		--		end
+		--	end
+		--
+		--	blocked = {}
+		--	for i = 1, count1 do
+		--		local unit = unitTable1[i]
+		--		local guid = UnitGUID(unit)
+		--		if guid then
+		--			local _, _, _, _, _, strid = strsplit("-", guid)
+		--			if strid and collection[strid] and not blocked[strid] then
+		--				blocked[strid] = true
+		--				local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
+		--				SendAddonMessage("Capping", format("%s:%.1f", strid, hp), "INSTANCE_CHAT")
+		--			end
+		--		end
+		--	end
+		--end
+		--
+		--function API:SetupHealthCheck(npcId, npcName, englishName, icon, color)
+		--	collection[npcId] = {npcName, englishName, icon, color}
+		--	if not started then
+		--		started = true
+		--		C_ChatInfo.RegisterAddonMessagePrefix("Capping")
+		--		mod:RegisterTempEvent("CHAT_MSG_ADDON", "HealthUpdate")
+		--		Timer(1, HealthScan)
+		--	end
+		--end
+		--
+		--function mod:HealthUpdate(prefix, msg, channel)
+		--	if prefix == "Capping" and channel == "INSTANCE_CHAT" then
+		--		local strid, strhp = strsplit(":", msg)
+		--		local hp = tonumber(strhp)
+		--		if strid and hp and collection[strid] and hp < 100.1 and hp > 0 then
+		--			if collection[strid].candyBarBar then
+		--				if hp < 100 then
+		--					reset[strid] = 0
+		--				end
+		--				collection[strid].candyBarBar:SetValue(hp)
+		--				collection[strid].candyBarDuration:SetFormattedText("%.1f%%", hp)
+		--			elseif hp < 100 then
+		--				local tbl = collection[strid]
+		--				local bar = mod:StartBar(tbl[1], 100, tbl[3], tbl[4], true)
+		--				bar:Pause()
+		--				bar.candyBarBar:SetValue(hp)
+		--				bar.candyBarDuration:SetFormattedText("%.1f%%", hp)
+		--				bar:Set("capping:customchat", function(bar)
+		--					if tbl[1] ~= tbl[2] then
+		--						return tbl[2] .."/".. tbl[1] .." - ".. bar.candyBarDuration:GetText()
+		--					else
+		--						return tbl[1] .." - ".. bar.candyBarDuration:GetText()
+		--					end
+		--				end)
+		--				bar:Set("capping:hpdata", tbl)
+		--				reset[strid] = 0
+		--				collection[strid] = bar
+		--			end
+		--		end
+		--	end
+		--end
+	end
+
+	do
 		local mods = {}
 		function mod:NewMod(name)
 			local t = {}
---
+++ Modules/IsleOfConquest.lua
@@ -0,0 +1,260 @@
+
+local mod, L
+do
+	local _, core = ...
+	mod, L = core:NewMod("Isle of Conquest")
+end
+
+local baseGateHealth = 1946880
+local lowestAllianceHp, lowestHordeHp = baseGateHealth, baseGateHealth
+local hordeGates, allianceGates = {}, {}
+local hordeGateBar, allianceGateBar = nil, nil
+local englishNames = {
+	["195494"] = "Horde Gate (Front)/",
+	["195495"] = "Horde Gate (West)/",
+	["195496"] = "Horde Gate (East)/",
+	["195698"] = "Alliance Gate (Front)/",
+	["195699"] = "Alliance Gate (West)/",
+	["195700"] = "Alliance Gate (East)/",
+}
+
+do
+	local CombatLogGetCurrentEventInfo = CombatLogGetCurrentEventInfo
+	function mod:COMBAT_LOG_EVENT_UNFILTERED()
+		local _, event, _, _, _, _, _, destGUID, _, _, _, _, _, _, amount = CombatLogGetCurrentEventInfo()
+		if event == "SPELL_BUILDING_DAMAGE" then
+			local _, _, _, _, _, strid = strsplit("-", destGUID)
+			if hordeGates[strid] then
+				local newHp = hordeGates[strid] - amount
+				hordeGates[strid] = newHp
+				if newHp < lowestHordeHp then
+					lowestHordeHp = newHp
+					local bar = hordeGateBar
+					if bar then
+						local hp = newHp / baseGateHealth * 100
+						if hp < 0.5 then
+							bar:Stop()
+						else
+							bar.candyBarBar:SetValue(hp)
+							bar.candyBarDuration:SetFormattedText("%.1f%%", hp)
+							local gate = strid == "195494" and L.front or strid == "195495" and L.west or L.east
+							bar.candyBarLabel:SetFormattedText(L.gatePosition, L.hordeGate, gate)
+							bar:Set("capping:englishprint", englishNames[strid])
+						end
+					end
+				end
+			elseif allianceGates[strid] then
+				local newHp = allianceGates[strid] - amount
+				allianceGates[strid] = newHp
+				if newHp < lowestAllianceHp then
+					lowestAllianceHp = newHp
+					local bar = allianceGateBar
+					if bar then
+						local hp = newHp / baseGateHealth * 100
+						if hp < 0.5 then
+							bar:Stop()
+						else
+							bar.candyBarBar:SetValue(hp)
+							bar.candyBarDuration:SetFormattedText("%.1f%%", hp)
+							local gate = strid == "195698" and L.front or strid == "195699" and L.west or L.east
+							bar.candyBarLabel:SetFormattedText(L.gatePosition, L.allianceGate, gate)
+							bar:Set("capping:englishprint", englishNames[strid])
+						end
+					end
+				end
+			end
+		elseif event == "UNIT_DIED" then
+			local _, _, _, _, _, strid = strsplit("-", destGUID)
+			if strid == "34776" or strid == "35069" then -- Alliance Siege, Horde Siege
+				SendAddonMessage("Capping", "rb", "INSTANCE_CHAT")
+			end
+		end
+	end
+end
+
+local function initGateBars()
+	mod:RegisterTempEvent("COMBAT_LOG_EVENT_UNFILTERED")
+	local aBar = mod:StartBar(L.allianceGate, 100, 2054277, "colorHorde", true) -- Interface/Icons/spell_tailor_defenceup01
+	aBar:Pause()
+	aBar.candyBarBar:SetValue(100)
+	aBar.candyBarDuration:SetText("100%")
+	aBar:Set("capping:englishprint", "Alliance Gate/")
+	aBar:Set("capping:customchat", function(bar)
+		if L.allianceGate ~= "Alliance Gate" then
+			return bar:Get("capping:englishprint") .. bar.candyBarLabel:GetText() .." - ".. bar.candyBarDuration:GetText()
+		else
+			return bar.candyBarLabel:GetText() .." - ".. bar.candyBarDuration:GetText()
+		end
+	end)
+	local hBar = mod:StartBar(L.hordeGate, 100, 2054277, "colorAlliance", true) -- Interface/Icons/spell_tailor_defenceup01
+	hBar:Pause()
+	hBar.candyBarBar:SetValue(100)
+	hBar.candyBarDuration:SetText("100%")
+	hBar:Set("capping:englishprint", "Horde Gate/")
+	hBar:Set("capping:customchat", function(bar)
+		if L.hordeGate ~= "Horde Gate" then
+			return bar:Get("capping:englishprint") .. bar.candyBarLabel:GetText() .." - ".. bar.candyBarDuration:GetText()
+		else
+			return bar.candyBarLabel:GetText() .." - ".. bar.candyBarDuration:GetText()
+		end
+	end)
+	allianceGateBar, hordeGateBar = aBar, hBar
+end
+
+local IoCSyncRequest
+do
+	local timer = nil
+	local NewTicker = C_Timer.NewTicker
+	local function SendIoCGates()
+		timer = nil
+		if IsInGroup(2) then -- We've not just ragequit
+			local msg = format(
+				"195494:%d:195495:%d:195496:%d:195698:%d:195699:%d:195700:%d",
+				hordeGates["195494"], hordeGates["195495"], hordeGates["195496"],
+				allianceGates["195698"], allianceGates["195699"], allianceGates["195700"]
+			)
+			SendAddonMessage("Capping", msg, "INSTANCE_CHAT")
+		end
+	end
+
+	local hereFromTheStart, hasData = true, true
+	local stopTimer = nil
+	local function allow() hereFromTheStart = false end
+	local function stop() hereFromTheStart = true stopTimer = nil end
+	function IoCSyncRequest()
+		for i = 1, 80 do
+			local scoreTbl = GetScoreInfo(i)
+			if scoreTbl and scoreTbl.damageDone and scoreTbl.damageDone ~= 0 then
+				hereFromTheStart = true
+				hasData = false
+				Timer(0.5, allow)
+				stopTimer = NewTicker(3, stop, 1)
+				SendAddonMessage("Capping", "gr", "INSTANCE_CHAT")
+				return
+			end
+		end
+
+		hereFromTheStart = true
+		hasData = true
+		initGateBars()
+	end
+
+	local me = UnitName("player").. "-" ..GetRealmName()
+	function mod:CHAT_MSG_ADDON(prefix, msg, channel, sender)
+		if prefix == "Capping" and channel == "INSTANCE_CHAT" then
+			self:HealthUpdate(prefix, msg, channel, sender)
+			if msg == "gr" and sender ~= me then -- gate request
+				if hasData then -- Joined a late game, don't send data
+					if timer then timer:Cancel() end
+					timer = NewTicker(1, SendIoCGates, 1)
+				elseif stopTimer then
+					stopTimer:Cancel()
+					stopTimer = NewTicker(3, stop, 1)
+				end
+			elseif msg == "rb" or msg == "rbh" then -- Re-Build / Re-Build Halfway
+				local pois = GetAreaPOIForMap(169)
+				for i = 1, #pois do
+					local tbl = GetAreaPOIInfo(169, pois[i])
+					local icon = tbl.textureIndex
+					if icon == 136 or icon == 138 then -- Workshop in IoC
+						local text = GetSpellInfo(56661) -- Build Siege Engine
+						local bar = self:GetBar(text)
+						if not bar then
+							self:StartBar(text, msg == "rb" and 181 or 90.5, 252187, icon == 136 and "colorAlliance" or "colorHorde") -- 252187 = ability_vehicle_siegeengineram
+						end
+					end
+				end
+			elseif not hereFromTheStart and sender ~= me then
+				local h1, h1hp, h2, h2hp, h3, h3hp, a1, a1hp, a2, a2hp, a3, a3hp = strsplit(":", msg)
+				local hGate1, hGate2, hGate3, aGate1, aGate2, aGate3 = tonumber(h1hp), tonumber(h2hp), tonumber(h3hp), tonumber(a1hp), tonumber(a2hp), tonumber(a3hp)
+				if hGate1 and hGate2 and hGate3 and aGate1 and aGate2 and aGate3 and -- Safety dance
+				h1 == "195494" and h2 == "195495" and h3 == "195496" and a1 =="195698" and a2 == "195699" and a3 == "195700" then
+					hereFromTheStart = true
+					hasData = true
+					initGateBars()
+					lowestHordeHp = math.min(hGate1, hGate2, hGate3)
+					lowestAllianceHp = math.min(aGate1, aGate2, aGate3)
+					hordeGates["195494"] = hGate1
+					hordeGates["195495"] = hGate2
+					hordeGates["195496"] = hGate3
+					allianceGates["195698"] = aGate1
+					allianceGates["195699"] = aGate2
+					allianceGates["195700"] = aGate3
+
+					local bar = hordeGateBar
+					if bar then
+						local hp = lowestHordeHp / baseGateHealth * 100
+						if hp < 1 then
+							bar:Stop()
+						else
+							bar.candyBarBar:SetValue(hp)
+							bar.candyBarDuration:SetFormattedText("%.1f%%", hp)
+							if lowestHordeHp ~= baseGateHealth then
+								local gate = lowestHordeHp == hGate1 and h1 or lowestHordeHp == hGate2 and h2 or h3
+								bar.candyBarLabel:SetFormattedText(L.gatePosition, L.hordeGate, gate == h1 and L.front or gate == h2 and L.west or L.east)
+								bar:Set("capping:englishprint", englishNames[gate])
+							end
+						end
+					end
+					local bar = allianceGateBar
+					if bar then
+						local hp = lowestAllianceHp / baseGateHealth * 100
+						if hp < 1 then
+							bar:Stop()
+						else
+							bar.candyBarBar:SetValue(hp)
+							bar.candyBarDuration:SetFormattedText("%.1f%%", hp)
+							if lowestAllianceHp ~= baseGateHealth then
+								local gate = lowestAllianceHp == aGate1 and a1 or lowestAllianceHp == aGate2 and a2 or a3
+								bar.candyBarLabel:SetFormattedText(L.gatePosition, L.allianceGate, gate == a1 and L.front or gate == a2 and L.west or L.east)
+								bar:Set("capping:englishprint", englishNames[gate])
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg:find(L.broken, nil, true) then
+		SendAddonMessage("Capping", "rb", "INSTANCE_CHAT")
+	elseif msg:find(L.halfway, nil, true) then
+		SendAddonMessage("Capping", "rbh", "INSTANCE_CHAT")
+	end
+end
+
+do
+	local RequestBattlefieldScoreData = RequestBattlefieldScoreData
+	function mod:EnterZone()
+		lowestAllianceHp, lowestHordeHp = baseGateHealth, baseGateHealth
+		hordeGates = {
+			["195494"] = baseGateHealth,
+			["195495"] = baseGateHealth,
+			["195496"] = baseGateHealth,
+		}
+		allianceGates = {
+			["195698"] = baseGateHealth,
+			["195699"] = baseGateHealth,
+			["195700"] = baseGateHealth,
+		}
+		--self:SetupAssault(61, 169)
+		--self:SetupHealthCheck("34922", L.hordeBoss, "Horde Boss", 236452, "colorAlliance") -- Overlord Agmar -- Interface/Icons/Achievement_Character_Orc_Male
+		--self:SetupHealthCheck("34924", L.allianceBoss, "Alliance Boss", 236448, "colorHorde") -- Halford Wyrmbane -- Interface/Icons/Achievement_Character_Human_Male
+		C_ChatInfo.RegisterAddonMessagePrefix("Capping")
+		self:RegisterEvent("CHAT_MSG_ADDON")
+		self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+		RequestBattlefieldScoreData()
+		self:Timer(1, RequestBattlefieldScoreData)
+		self:Timer(2, IoCSyncRequest)
+	end
+end
+
+function mod:ExitZone()
+	self:UnregisterEvent("CHAT_MSG_ADDON")
+	self:UnregisterEvent("CHAT_MSG_MONSTER_YELL")
+	self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+end
+
+mod:RegisterZone(628)
