c254ac99abbb6136b0f0e82551ece0f6b1a30269
Matthi3u <Matthi3u@users.noreply.github.com>
2020-12-22 02:48:34 +0100

    Update frFR (#93)

--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -9,15 +9,15 @@ L.flagRespawns = "Réapparition drapeau(x)"
 
 L.takenTheFlagTrigger = "^(.+) a pris le drapeau !"
 L.hasTakenTheTrigger = "s'est emparée"
---L.upgradeToTrigger = "Upgrade to"
+L.upgradeToTrigger = "Passé à"
 L.droppedTrigger = "a été lâché"
 L.capturedTheTrigger = "a pris le drapeau de"
 
 L.hordeGate = "Porte de la Horde"
 L.allianceGate = "Porte de l'Alliance"
---L.gatePosition = "%s (%s)"
+L.gatePosition = "%s (%s)"
 L.west = "Ouest"
---L.front = "Front"
+L.front = "Devant"
 L.east = "Est"
 L.hordeBoss = "Chef de la Horde"
 L.allianceBoss = "Chef de l'Alliance"
@@ -25,20 +25,20 @@ L.galvangar = "Galvangar" -- Capitaine Galvangar
 L.balinda = "Balinda" -- Capitaine Balinda Gîtepierre
 L.ivus = "Ivus" -- Ivus le Seigneur de la forêt
 L.lokholar = "Lokholar" -- Lokholar le Seigneur des glaces
---L.handIn = "|cFF33FF99Capping|r: Automatically handing in quest items."
+L.handIn = "|cFF33FF99Capping|r: Remise automatique des objets de quête."
 
 --- Alliance IoC Workshop yells:
--- Gnomish Mechanic yells: I'm halfway there! Keep the Horde away from here.  They don't teach fighting in engineering school!
--- Gnomish Mechanic yells: It's broken already?! No worries. It's nothing I can't fix.
+-- Mécano gnome crie : J'en suis à la moitié ! Tenez la Horde à distance. On n'apprend pas à se battre dans les écoles d'ingénieurs !
+-- Mécano gnome crie : Déjà cassé ?! Pas de souci. Ce n'est que je ne puisse pas réparer.
 --- Horde IoC Workshop yells:
--- Goblin Mechanic yells: I'm about halfway done! Keep the Alliance away - fighting's not in my contract!
--- Goblin Mechanic yells: It's broken again?! I'll fix it... just don't expect the warranty to cover this.
---L.halfway = "halfway"
---L.broken = "broken"
+-- Mécano goblin crie : J'en suis à la moitié ! Tenez l'alliance à distance - combattre n'est pas dans mon contrat !
+-- Mécano goblin crie : C'est déjà cassé ?! Je le répare... Ne pensez pas que la garantie va couvrir ça.
+L.halfway = "moitié"
+L.broken = "cassé"
 
 -- Wintergrasp
---L.damaged = "|cFF33FF99Capping|r: %s Damaged"
---L.destroyed = "|cFF33FF99Capping|r: %s Destroyed"
+L.damaged = "|cFF33FF99Capping|r: %s endommagé"
+L.destroyed = "|cFF33FF99Capping|r: %s détruit"
 L.northEastKeep = "Tour de la Forteresse Nord-Est"
 L.southEastKeep = "Tour de la Forteresse Sud-Est"
 L.northWestKeep = "Tour de la Forteresse Nord-Ouest"
