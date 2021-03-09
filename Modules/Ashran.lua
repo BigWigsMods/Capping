fa6132c5595cca3c2ce808c4ebe0d5bb201cc6ba
funkydude <funkydude@users.noreply.github.com>
2021-03-09 20:49:37 +0000

    Modules/Ashran: Add health tracking for the mages, closes #95

--- Modules/Ashran.lua
+++ Modules/Ashran.lua
@@ -14,7 +14,7 @@ do
 		if target then
 			local _, _, _, _, _, id = strsplit("-", target)
 			local mobId = tonumber(id)
-			if mobId == 81870 or mobId == 83830 then -- Anenga (Alliance) / Kalgan (Horde)
+			if mobId == 81870 or mobId == 82204 then -- Anenga (Alliance) / Atomik (Horde)
 				local tbl = GetCurrencyInfo(944) -- Artifact Fragment
 				if tbl and tbl.quantity > 0 and GetNumGossipOptions() == 3 then -- Have the currency and boss isn't already summoned
 					SelectGossipOption(1)
@@ -31,6 +31,8 @@ end
 function mod:EnterZone()
 	hasPrinted = false
 	self:RegisterEvent("GOSSIP_SHOW")
+	self:SetupHealthCheck("88178", L.hordeGuardian, "Horde Guardian", 236440, "colorAlliance") -- Jeron Emberfall -- Interface/Icons/achievement_character_bloodelf_male
+	self:SetupHealthCheck("88224", L.allianceGuardian, "Alliance Guardian", 236447, "colorHorde") -- Rylai Crestfall -- Interface/Icons/Achievement_character_human_female
 end
 
 function mod:ExitZone()
