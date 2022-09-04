8a87e09ce6eed30d212489232714752243dfedf6
funkydude <funkydude@users.noreply.github.com>
2022-09-04 20:28:30 +0100

    Fix AB timers on Wrath

---
+++ Modules/ArathiBasin_Wrath.lua
@@ -0,0 +1,27 @@
+
+local mod
+do
+	local _, core = ...
+	mod = core:NewMod()
+end
+
+local instanceIdToMapId = {
+	[529] = 1461, -- Arathi Basin Classic
+	--[2107] = 93, -- Arathi Basin
+	--[1681] = 837, -- Arathi Basin Snowy PvP Brawl
+	--[2177] = 1383, -- Arathi Basin Brawl Vs AI
+}
+
+function mod:EnterZone(id)
+	self:StartFlagCaptures(65, instanceIdToMapId[id])
+	self:StartScoreEstimator()
+end
+
+function mod:ExitZone()
+	self:StopScoreEstimator()
+	self:StopFlagCaptures()
+end
+
+for k in next, instanceIdToMapId do
+	mod:RegisterZone(k)
+end
