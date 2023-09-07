14ccb5d54785622a0d7814c8692f8ccf4b043438
funkydude <funkydude@users.noreply.github.com>
2023-09-07 02:22:21 +0100

    Modules/AlteracValley: Add another ID

--- Modules/AlteracValley_Wrath.lua
+++ Modules/AlteracValley_Wrath.lua
@@ -13,6 +13,7 @@ do
 		[30908] = true, -- alliance
 		[30909] = true, -- alliance
 		[97829] = true, -- alliance (classic era)
+		[97830] = true, -- alliance (classic era)
 		[35739] = true, -- horde
 		[35740] = true, -- horde
 		[35741] = true, -- horde
