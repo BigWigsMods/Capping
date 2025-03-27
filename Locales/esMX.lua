8fb1b9a6359fdaf1a68e9c63881802ae6eb2a06f
milotic <al0305@hotmail.com>
2025-03-27 12:49:44 -0600

    Update esMX.lua

--- Locales/esMX.lua
+++ Locales/esMX.lua
@@ -3,69 +3,69 @@ if GetLocale() ~= "esMX" then return end
 local _, mod = ...
 local L = mod.L
 
---L.battleBegins = "Battle Begins"
---L.finalScore = "Final: %d - %d"
---L.flagRespawns = "Flag respawns"
+L.battleBegins = "La batalla ha comenzado"
+L.finalScore = "Final: %d - %d"
+L.flagRespawns = "La bandera se ha restablecido"
 
---L.takenTheFlagTrigger = "^(.+) has taken the flag!"
---L.hasTakenTheTrigger = "has taken the"
---L.droppedTrigger = "dropped"
---L.capturedTheTrigger = "captured the"
+L.takenTheFlagTrigger = "^(.+) ha cogido la bandera!"
+L.hasTakenTheTrigger = "ha cogido"
+L.droppedTrigger = "soltado"
+L.capturedTheTrigger = "capturado"
 
---L.hordeGate = "Horde Gate"
---L.allianceGate = "Alliance Gate"
---L.gatePosition = "%s (%s)"
---L.west = "West"
---L.front = "Front"
---L.east = "East"
---L.hordeBoss = "Horde Boss"
---L.allianceBoss = "Alliance Boss"
---L.galvangar = "Galvangar"
---L.balinda = "Balinda"
+L.hordeGate = "Puerta de la Horda"
+L.allianceGate = "Puerta de la Alianza"
+L.gatePosition = "%s (%s)"
+L.west = "Oeste"
+L.front = "Frente"
+L.east = "Este"
+L.hordeBoss = "Jefe de la Horda"
+L.allianceBoss = "Jefe de la Alianza"
+L.galvangar = "Galvangar"
+L.balinda = "Balinda"
 L.ivus = "Ivus" -- Ivus el Señor del Bosque
 L.lokholar = "Lokholar" -- Lokholar el Señor del Hielo
---L.handIn = "|cFF33FF99Capping|r: Automatically handing in quest items."
-L.anchorTooltip = "|cffeda55fClic derecho|r para acceder a las opciones"
---L.anchorTooltipNote = "Open the options and lock the bars to hide this moving anchor."
+L.handIn = "|cFF33FF99Capping|r: Entregando objetos de misión automáticamente."
+L.anchorTooltip = "|cffeda55fRight-Clic|r para acceder a las opciones"
+L.anchorTooltipNote = "Abre las opciones y bloquea las barras para esconder este ancla móvil."
 
 --- Alliance IoC Workshop yells:
--- Gnomish Mechanic yells: I'm halfway there! Keep the Horde away from here.  They don't teach fighting in engineering school!
--- Gnomish Mechanic yells: It's broken already?! No worries. It's nothing I can't fix.
+-- Mecánico gnomo grita: ¡Estoy a medias! Mantén a la Horda alejada de aquí. ¡En la escuela de ingeniería no enseñan a luchar!
+-- Mecánico gnomo grita: ¿Ya está rota? No pasa nada. No es nada que no pueda arreglar.
 --- Horde IoC Workshop yells:
--- Goblin Mechanic yells: I'm about halfway done! Keep the Alliance away - fighting's not in my contract!
--- Goblin Mechanic yells: It's broken again?! I'll fix it... just don't expect the warranty to cover this.
---L.halfway = "halfway" -- Needs to match the in game text exactly
---L.broken = "broken" -- Needs to match the in game text exactly
+-- Mecánico goblin grita: ¡Ya casi estoy! Mantén a la Alianza alejada... ¡Luchar no entra en mi contrato!
+-- Mecánico goblin grita: ¿Está estropeada otra vez? Lo arreglaré... pero no esperes que la garantía cubra esto.
+L.halfway = "alejada" -- Needs to match the in game text exactly
+L.broken = "arreglar" -- Needs to match the in game text exactly
 
 -- Wintergrasp
---L.damaged = "|cFF33FF99Capping|r: %s Damaged"
---L.destroyed = "|cFF33FF99Capping|r: %s Destroyed"
---L.northEastKeep = "North-East Fortress Tower"
---L.southEastKeep = "South-East Fortress Tower"
---L.northWestKeep = "North-West Fortress Tower"
---L.southWestKeep = "South-West Fortress Tower"
---L.northWest = "North-West Wall"
---L.southWest = "South-West Wall"
---L.south = "South Wall"
---L.southEast = "South-East Wall"
---L.northEast = "North-East Wall"
---L.innerWest = "Inner-West Wall"
---L.innerSouth = "Inner-South Wall"
---L.innerEast = "Inner-East Wall"
---L.southGate = "South Gate"
---L.mainEntrance = "Main Entrance"
---L.westTower = "West Tower"
---L.southTower = "South Tower"
---L.eastTower = "East Tower"
+L.damaged = "|cFF33FF99Capping|r: %s Dañada"
+L.destroyed = "|cFF33FF99Capping|r: %s Destruida"
+L.northEastKeep = "Torre de la Fortaleza Noreste"
+L.southEastKeep = "Torre de la Fortaleza Sureste"
+L.northWestKeep = "Torre de la Fortaleza Noroeste"
+L.southWestKeep = "Torre de la Fortaleza Suroeste"
+L.northWest = "Muralla del Noroeste"
+L.southWest = "Muralla del Suroeste"
+L.south = "Muralla del Sur"
+L.southEast = "Muralla del Sureste"
+L.northEast = "Muralla del Noreste"
+L.innerWest = "Muralla interior Oeste"
+L.innerSouth = "Muralla interior Sur"
+L.innerEast = "Muralla interior Este"
+L.southGate = "Puerta del Sur"
+L.mainEntrance = "Entrada principal"
+L.westTower = "Torre del Oeste"
+L.southTower = "Torre del Sur"
+L.eastTower = "Torre del Este"
 
 -- Ashran
---L.hordeGuardian = "Horde Guardian"
---L.allianceGuardian = "Alliance Guardian"
+L.hordeGuardian = "Guardián de la Horda"
+L.allianceGuardian = "Guardián de la Alianza"
 L.kronus = "Kronus"
 L.fangraal = "Fangraal"
 
 -- Arena
---L.arenaStartTrigger = "The Arena battle has begun!" -- Needs to match the in game text exactly
---L.arenaStart60s = "One minute until the Arena battle begins!" -- Needs to match the in game text exactly
---L.arenaStart30s = "Thirty seconds until the Arena battle begins!" -- Needs to match the in game text exactly
---L.arenaStart15s = "Fifteen seconds until the Arena battle begins!" -- Needs to match the in game text exactly
+L.arenaStartTrigger = "¡La batalla de arena ha comenzado!" -- Needs to match the in game text exactly
+L.arenaStart60s = "¡Un minuto hasta que comience la batalla de arena!" -- Needs to match the in game text exactly
+L.arenaStart30s = "¡Treinta segundos hasta que comience la batalla de arena!" -- Needs to match the in game text exactly
+L.arenaStart15s = "¡Quince segundos para que comience la batalla de arena!" -- Needs to match the in game text exactly
