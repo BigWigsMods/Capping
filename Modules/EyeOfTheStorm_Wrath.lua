35ff9aaf7ca52768984cc49829480f8dcaedf4b8
funkydude <funkydude@users.noreply.github.com>
2025-05-13 22:05:05 +0100

    Modules/EyeOfTheStorm: Adjust time in solo mode

--- Modules/EyeOfTheStorm_Wrath.lua
+++ Modules/EyeOfTheStorm_Wrath.lua
@@ -77,7 +77,7 @@ do
 		--if id == 566 then -- Normal/Brawl
 		--	self:RegisterEvent("RAID_BOSS_WHISPER")
 		--else -- Rated
-		--	self:StartFlagCaptures(60, nil, colors)
+		--	self:StartFlagCaptures(C_PvP.IsSoloRBG() and 30 or 60, nil, colors)
 		--end
 	end
 end
