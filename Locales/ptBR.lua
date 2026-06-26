cefd6faa692a34abb53a7ccc2b05d370d1fb5f84
Marco Schneider <58363718+Marco-Schneider@users.noreply.github.com>
2026-06-25 23:10:20 -0300

    Update ptBR (#179)

--- Locales/ptBR.lua
+++ Locales/ptBR.lua
@@ -66,7 +66,7 @@ L.kronus = "Kronus"
 L.fangraal = "Fangraal"
 
 -- Arena
---L.arenaStartTrigger = "The Arena battle has begun!" -- Needs to match the in game text exactly
---L.arenaStart60s = "One minute until the Arena battle begins!" -- Needs to match the in game text exactly
---L.arenaStart30s = "Thirty seconds until the Arena battle begins!" -- Needs to match the in game text exactly
---L.arenaStart15s = "Fifteen seconds until the Arena battle begins!" -- Needs to match the in game text exactly
+L.arenaStartTrigger = "A batalha na Arena começou!" -- Needs to match the in game text exactly
+L.arenaStart60s = "Um minuto até a batalha na Arena começar!" -- Needs to match the in game text exactly
+L.arenaStart30s = "Trinta segundos até a batalha na Arena começar!" -- Needs to match the in game text exactly
+L.arenaStart15s = "Quinze segundos até a batalha na Arena começar!" -- Needs to match the in game text exactly
